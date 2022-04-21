1e156c1fa4892783370471c71bc33eb6fd04f1b5
Joao Victor <jov9051@outlook.com>
2022-04-21 16:53:31 -0300

    feat: uso percentual de memoria criado em index.js

--- src/index.js
+++ src/index.js
@@ -6,4 +6,6 @@ setInterval(() => {
     let totalMemory = parseInt(totalmem() / 1024 / 1024);
 
     let freeMemory = parseInt(freemem() / 1024 / 1024);
+
+    let percentualMemoryUsage = parseInt((freeMemory / totalMemory) * 100);
 }, 1000);
