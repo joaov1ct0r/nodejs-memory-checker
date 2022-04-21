2da6bb53663216c552ea9f8ec1ead0a6391101be
Joao Victor <jov9051@outlook.com>
2022-04-21 17:00:27 -0300

    feat: sistema apresentando dados sobre memoria em mb

--- src/index.js
+++ src/index.js
@@ -8,4 +8,13 @@ setInterval(() => {
     let freeMemory = parseInt(freemem() / 1024 / 1024);
 
     let percentualMemoryUsage = parseInt((freeMemory / totalMemory) * 100);
+
+    let stats = {
+        freeMemory: `${freeMemory} MB`,
+        totalMemory: `${totalMemory} MB`,
+        percentualMemoryUsage: `${percentualMemoryUsage}%`
+    };
+
+    console.log('========== MEMORY STATUS ==========');
+    console.table(stats);
 }, 1000);
